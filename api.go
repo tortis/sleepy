af628a71a873e7a97a3ea64633eb0cf4ebec0c8f
David Findley <thefindley@gmail.com>
2015-04-29 13:47:09 -0400

    Working on API

--- api.go
+++ api.go
@@ -1,5 +1,30 @@
 package sleepy
 
+import "github.com/gorilla/mux"
+
 type API struct {
-	resources []Resource
+	resources []*Resource
+	router    *mux.Router
+}
+
+func New() *API {
+	return &API{
+		resources: make([]*Resource, 0),
+		router:    mux.NewRouter(),
+	}
+}
+
+func (api *API) GetRouter() *mux.Router {
+	return api.router
+}
+
+// All of the resources calls need to be added to the router
+func (api *API) Register(r *Resource) {
+	// Add the resource to our local list
+	api.resource = append(api.resources, r)
+
+	// Attach handler for each call
+	for _, call := range r.calls {
+
+	}
 }
